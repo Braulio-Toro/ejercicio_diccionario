90e27fedcafd3fdaa3fb891f0f36187c6f629e7e
Braulio-Toro <br.toropalma@duocuc.cl>
2025-06-02 12:04:09 -0400

    Añadir diccionario y lista

--- ej_list.py
+++ ej_list.py
@@ -4,14 +4,23 @@ menu = f"""--- MENÚ ---
 2) Ver alumnos
 3) Salir
 {"-"*20}"""
-
+alumnos=[]
 while True:
     os.system("cls || clear")
     print(menu)
     opt=input("Ingrese opción: ")
     os.system("cls || clear")
     if opt=="1":
-        pass
+        print("AGREGAR ALUMNO")
+        codigo=input("Ingrese un código: ")
+        edad=int(input("Ingrese edad: "))
+        nombre=input("Ingrese nombre: ")
+        alumno={
+            "codigo":codigo,
+            "edad":edad,
+            "nombre":nombre
+        }
+        alumnos.append(alumno)
     elif opt=="2":
         pass
     elif opt=="3":
